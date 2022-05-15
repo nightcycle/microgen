ad693958b30c5416cc33c031e38af7050aafb247
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-05-14 22:29:12 -0400

    yeah this first attempt at a tree aint great

--- micros/Tree.micro.lua
+++ micros/Tree.micro.lua
@@ -1,15 +1,25 @@
 local packages = script.Parent.Parent
 
 return function(microgen, inst: Part)
+
+
+
 	local config = inst:GetAttributes()
 	config.TrunkWidth = config.TrunkWidth or 2
 	config.TrunkMaterial = config.TrunkMaterial or "Wood"
 	config.TrunkMaterialVariant = config.TrunkMaterialVariant or nil
 	config.TrunkColor = config.TrunkColor or Color3.fromHSV(0.1,0.5,0.5)
+	config.LeafColor = config.LeafColor or Color3.fromHSV(0.35, 0.9, 0.6)
+	config.LeafMaterial = config.LeafMaterial or "Grass"
+	config.LeafVariant = config.LeafVariant or nil
+	config.LeafCount = config.LeafCount or 3
+	config.LeafFloor = config.LeafFloor or 0.4
 	config.TreeAngle = config.TreeAngle or 30
 	config.Segments = config.Segments or 6
 	config.Gravity = config.Gravity or 1
 	config.SplitChance = config.SplitChance or 0.25
+	config.Seed = config.Seed or tonumber(config.MicromanagerId or "") or 0
+	local rand = Random.new(config.Seed)
 
 	for k, v in pairs(config) do inst:SetAttribute(k, v) end
 
@@ -19,6 +29,7 @@ return function(microgen, inst: Part)
 	local treeHeight = inst.Size.Y
 	local treeWidth = math.min(inst.Size.X, inst.Size.Z)
 	local trunkHeight = treeHeight--math.clamp(treeHeight - treeWidth*0.5, 0.5, treeHeight)
+	local leafDiameter = treeHeight - treeHeight*config.LeafFloor
 
 	local rootCF = inst.CFrame * CFrame.new(0,-inst.Size.Y/2,0)
 
@@ -29,8 +40,8 @@ return function(microgen, inst: Part)
 		-- local lV = -startCF.LookVector
 
 		local widthWeight = 1-((index-1)/config.Segments)
-		local xAngle = math.rad(config.TreeAngle * math.random() - config.TreeAngle*0.5)*2
-		local yAngle = math.rad(config.TreeAngle * math.random() - config.TreeAngle*0.5)*2
+		local xAngle = math.rad(config.TreeAngle * rand:NextNumber() - config.TreeAngle*0.5)*2
+		local yAngle = math.rad(config.TreeAngle * rand:NextNumber() - config.TreeAngle*0.5)*2
 		local cf = startCF * CFrame.Angles(xAngle,yAngle,0)
 		local gravityLV = -((position + startCF.LookVector - Vector3.new(0,config.Gravity*(1-widthWeight),0)) - position).Unit
 		local rV = gravityLV:Cross(inst.CFrame.LookVector)
@@ -60,10 +71,11 @@ return function(microgen, inst: Part)
 		
 		if config.Segments > index then
 			newTrunk(CFrame.fromMatrix(finalTarget, rV, uV), index + 1)
-			if config.SplitChance > math.random() then
+			if config.SplitChance > rand:NextNumber() then
 				newTrunk(CFrame.fromMatrix(finalTarget, rV, uV), index + 1)
 			end
 		end
+
 	end
 
 	local trunkJoint = Instance.new("Part", model)
@@ -76,6 +88,21 @@ return function(microgen, inst: Part)
 	trunkJoint.Parent = model
 
 	newTrunk(rootCF*CFrame.Angles(math.rad(-90),0,0), 1)
+
+	local centerCF = inst.CFrame * CFrame.new(0,inst.Size.Y/2, 0) * CFrame.new(0,-leafDiameter/2,0)
+	for i=1, config.LeafCount do
+		local x = rand:NextNumber() * leafDiameter * 0.5 * if rand:NextNumber() < 0.5 then 1 else -1
+		local y = rand:NextNumber() * leafDiameter * 0.5 * if rand:NextNumber() < 0.5 then 1 else -1
+		local z = rand:NextNumber() * leafDiameter * 0.5 * if rand:NextNumber() < 0.5 then 1 else -1
+		local size = Vector3.new(leafDiameter - math.abs(x), leafDiameter - math.abs(y), leafDiameter - math.abs(z))
+		local leafPart =  Instance.new("Part", model)
+		leafPart.Name = "Leaf"
+		leafPart.Color = config.LeafColor
+		leafPart.Material = config.LeafMaterial
+		leafPart.CFrame = centerCF *  CFrame.new(x*0.5,y*0.5,z*0.5)
+		leafPart.Shape = Enum.PartType.Ball
+		leafPart.Size = size
+	end
 	
 	model.Parent = inst.Parent
 	
